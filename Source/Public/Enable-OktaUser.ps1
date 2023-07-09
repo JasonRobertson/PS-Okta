93c4c7aaec02544221f2ee84a72b8c867c72f0c3
Jason Robertson <jason@mxcloud.services>
2023-07-08 20:20:54 -0400

    Refactor of Enable-OktaUser

--- Source/Public/Enable-OktaUser.ps1
+++ Source/Public/Enable-OktaUser.ps1
@@ -3,30 +3,16 @@ function Enable-OktaUser {
   param (
     # Identity is used to fetch a user by id, login, or login shortname if the short name is unambiguous
     [parameter(ParameterSetName='UserID')]
-    [string]$Identity
-    # ApiToken is a required field and can be created in Okta instance https://{domain}.okta.com/admin/access/api/tokens
+    [string]$Identity,
+    [switch]$SendEmail
   )
-  begin {
-    #region Static Variables
+  $oktaAPI                = [hashtable]::new()
+  $oktaAPI.All            = $all
+  $oktaAPI.Method         = 'POST'
+  $oktaAPI.Body           = [hashtable]::new()
+  $oktaAPI.Body.sendEmail = $SendEmail
 
-    $oktaAPI          = [hashtable]::new()
-    $oktaAPI.Method   = 'GET'
-    $oktaAPI.Body     = $body
-    $oktaAPI.All      = $all
-    $oktaAPI.Endpoint = $Endpoint
+  $oktaAPI.Endpoint = "users/$oktaUserID/lifecycle/activate"
 
-
-    #region Build the Web Request
-    $webRequest                 = [hashtable]::new()
-    $webRequest.Uri             = "$oktaUrl/users/$oktaUserID/lifecycle/activate?sendEmail=false"
-    $webRequest.Body            = $body
-    $webRequest.Method          = 'GET'
-    $webRequest.Headers         = $headers
-    $webRequest.UseBasicParsing = $true
-    #endregion
-  }
-  process {
-    
-  }
-  end{}
+  Invoke-OktaAPI @oktaAPI
 }
