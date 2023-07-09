479ab7b0e783180b738864ab3c2cb70c7fded6bb
Jason Robertson <jason@mxcloud.services>
2023-07-08 21:26:09 -0400

    Refactor Get-OktaUserFactor to include parameters -- FactorProvider -- Status

--- Source/Public/Get-OktaUserFactor.ps1
+++ Source/Public/Get-OktaUserFactor.ps1
@@ -2,28 +2,30 @@ function Get-OktaUserFactor {
   [CmdletBinding(DefaultParameterSetName='Default')]
   param (
     # Identity is used to fetch a user by id, login, or login shortname if the short name is unambiguous
-    [parameter(Mandatory, ValueFromPipeline=$true)]
+    [parameter(Mandatory, ValueFromPipeline=$true, Position=0)]
     [alias('id')]
     [string[]]$Identity,
+    [ValidateSet('ACTIVE','DISABLED','ENROLLED','EXPIRED','INACTIVE','NOT_SETUP','PENDING_ACTIVATION')]
+    $Status,
     [ValidateSet('CUSTOM','DUO','FIDO','GOOGLE','OKTA','RSA','SYMANTEC','YUBICO')]
-    $FactorType
+    $FactorProvider
+
   )
-  begin {
-    $oktaAPI        = [hashtable]::new()
-    $oktaAPI.Method = 'GET'
-    $oktaAPI.Body   = $body
-    $oktaAPI.All    = $all
-
-
-  }
-  process {
-    foreach ($userID in $identity) {
-      $id = (Invoke-OktaAPI -EndPoint users/$userID).id
-      $oktaAPI.Endpoint = "users/$id/factors"
-      Invoke-OktaAPI @oktaAPI
+  foreach ($userID in $identity) {
+    $id = (Invoke-OktaAPI -EndPoint users/$userID).id
+    $apiResponse = Invoke-OktaAPI -EndPoint "users/$id/factors"
+    
+    if ($FactorProvider -and $Status) {
+      $apiResponse.Where({$_.Provider -eq $FactorProvider -and $_.Status -eq $status})
     }
-  }
-  end {
-    [system.gc]::Collect();
+    elseif ($status){ 
+      $apiResponse.Where({$_.Status -eq $status})
+    }
+    elseif ($FactorProvider) {
+      $apiResponse.Where({$_.Provider -eq $FactorProvider})
+    }
+    else {
+      $apiResponse
+    }
   }
 }
