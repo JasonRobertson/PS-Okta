912e05eb86209b9fdf6c47aa36f551bc58e70bf2
Jason Robertson <jason@mxcloud.services>
2023-08-03 23:11:53 -0400

    Developed Reset-OktaUserMultifactor command

---
+++ Source/Public/Reset-OktaUserMultiFactor.ps1
@@ -0,0 +1,25 @@
+
+function Reset-OktaUserMultiFactor {
+  [CmdletBinding(DefaultParameterSetName='Default')]
+  param (
+    [parameter(Mandatory)]
+    [string]$Identity,
+    [parameter(Mandatory, ParameterSetName='Provider')]
+    [ValidateSet('CUSTOM','DUO','FIDO','GOOGLE','OKTA','RSA','SYMANTEC','YUBICO')]
+    [string]$Provider,
+    [parameter(ParameterSetName='Provider')]
+    [switch]$RemoveRecoveryEnrollment,
+    [parameter(ParameterSetName='Default')]
+    [switch]$All
+  )
+  $factorID = if ($provider) {(Get-OktaUserFactor -Identity $Identity -Provider $Provider).id}
+  $endPoint = switch ($PSCmdlet.ParameterSetName) {
+    Default  {"/users/$identity/lifecycle/reset_factors"}
+    Provider {"/users/$identity/factors/$factorID?removeRecoveryEnrollment=$RemoveRecoveryEnrollment"}
+  }
+  $oktaAPI          = [hashtable]::new()
+  $oktaAPI.Method   = 'POST'
+  $oktaAPI.Endpoint = $endPoint
+
+  Invoke-OktaAPI @oktaAPI
+}
