0d4992b7514530f27e1eb64918cb4e6a02c320d9
Jason Robertson <jason@mxcloud.services>
2023-08-04 00:01:20 -0400

    Helper function Write-OktaError Modified Get-OktaUserMultiFactor to use Write-OktaError

--- Source/Public/Get-OktaUserMultiFactor.ps1
+++ Source/Public/Get-OktaUserMultiFactor.ps1
@@ -5,26 +5,33 @@ function Get-OktaUserMultiFactor {
     [parameter(Mandatory, ValueFromPipeline=$true, Position=0)]
     [alias('id')]
     [string[]]$Identity,
+    [parameter(ParameterSetName='Status')]
     [ValidateSet('ACTIVE','DISABLED','ENROLLED','EXPIRED','INACTIVE','NOT_SETUP','PENDING_ACTIVATION')]
     $Status,
+    [parameter(ParameterSetName='Provider')]
     [ValidateSet('CUSTOM','DUO','FIDO','GOOGLE','OKTA','RSA','SYMANTEC','YUBICO')]
     $Provider
   )
   foreach ($userID in $identity) {
     $id = (Invoke-OktaAPI -EndPoint users/$userID).id
-    $apiResponse = Invoke-OktaAPI -EndPoint "users/$id/factors"
-    
-    if ($Provider -and $Status) {
-      $apiResponse.Where({$_.Provider -eq $Provider -and $_.Status -eq $status})
-    }
-    elseif ($status){ 
-      $apiResponse.Where({$_.Status -eq $status})
-    }
-    elseif ($Provider) {
-      $apiResponse.Where({$_.Provider -eq $Provider})
-    }
-    else {
-      $apiResponse
+
+    if ($id) {
+      $apiResponse = Invoke-OktaAPI -EndPoint "users/$id/factors"
+      $response = switch ($pscmdlet.ParameterSetName) {
+        Status   {$apiResponse.Where({$_.Status -eq $status})}
+        Provider {$apiResponse.Where({$_.Provider -eq $Provider})}
+        Default  {$apiResponse}
+      }
+      switch ($null -ne $response) {
+        True  {$response}
+        False {
+          $message = switch ($pscmdlet.ParameterSetName) {
+            Status   {"No Factor found with Status: '$status'"}
+            Provider {"No Factor found with Provider: '$provider'"}
+          }
+          Write-OktaError -Message $message
+        }
+      }
     }
   }
 }
