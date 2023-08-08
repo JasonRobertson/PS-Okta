798ea483b0bd74bf58b36b5f4fc441f8ef1745f2
Jason Robertson <jason@mxcloud.services>
2023-08-08 13:12:25 -0400

    Refactor Get-OktaAPP Removed excessive code Simplified the query logic Added the capability to use Asterik in Identity parameter

--- Source/Public/Get-OktaApp.ps1
+++ Source/Public/Get-OktaApp.ps1
@@ -1,102 +1,33 @@
 function Get-OktaApp {
-  [CmdletBinding(DefaultParameterSetName='Limit')]
+  [CmdletBinding()]
   param (
-    [parameter(ParameterSetName='AppID')]
-    [alias('AppID')]
-    [string]$ID,
-    [alias('AppName')]
-    [string]$Name,
+    [string]$Identity,
     [ValidateSet('Active', 'Inactive')]
     $Status,
-    [parameter(ParameterSetName='Limit')]
     [ValidateRange(1,500)]
-    [int]$Limit,
-    [parameter(ParameterSetName='Limit')]
+    [int]$Limit = 500,
     [switch]$All
   )
-  begin {
-    Write-Verbose "BEGIN Block: Start"
-    #Verify the connection has been established
-    $oktaUrl = Test-OktaConnection
-    #region Build the headers
-    $headers                  = [hashtable]::new()
-    $headers.Accept           = 'application/json'
-    $headers.Authorization    = Convert-OktaAPIToken
-    #endregion
+  $filter = switch ($status) {
+    Active   {'status eq "ACTIVE"'}
+    Inactive {'status eq "INACTIVE"'}
+  }
 
-    Write-Verbose "OktaDomain: $oktaDomain"
-    #region Build Headers
-    Write-Verbose "Build Headers: Start"
-    $headers                  = [hashtable]::new()
-    $headers.Accept           = 'application/json'
-    $headers.Authorization    = Convert-OktaAPIToken
-    Write-Verbose "Build Headers: Complete"
-    #endregion
-    #region Build Body
-    $body       = [hashtable]::new()
-    $body.limit = switch ($Limit -ge 1 -or $limit -gt 200){
-                    True  {$Limit}
-                    False {200}
-                  }
-    if ($status){
-      $body.filter = switch ($status) {
-        Active   {'status eq "ACTIVE"'}
-        Inactive {'status eq "INACTIVE"'}
-      }
-    }
-    #endregion
-    #region Build Web Request
-    $webRequest                 = [hashtable]::new()
-    $webRequest.Uri             = switch ($pscmdlet.ParameterSetName){
-                                    Limit {"$oktaUrl/apps/"}
-                                    AppID {"$oktaUrl/apps/$ID"}
-                                  }
-    $webRequest.Method          = 'GET'
-    $webRequest.Headers         = $headers
-    $webRequest.UseBasicParsing = $true
-    $webRequest.Body            = $body
-    #endregion
-    Write-Verbose "BEGIN Block: End"
+  $query = switch ([wildcardpattern]::ContainsWildcardCharacters($identity)) {
+    True  {$Identity.Replace('*','')}
+    False {$Identity}
   }
-  process {
-    Write-Verbose "PROCESS Block: Start"
-    switch ($all) {
-      True {
-        Write-Verbose "All Parameter Present: True"
-        do{
-          $response = Invoke-WebRequest @webRequest
-          $webRequest.Uri = $response.RelationLink.next #RelationLink is the recommended approach for pagination from Okta.
-          $webRequest.Remove('Body')
-          (ConvertFrom-Json $response.Content)
-          Write-Verbose "NextLink present: True"
-        } until (-not $response.RelationLink.next)
-        Write-Verbose "NextLink present: False"
-      }
-      False {
-        Write-Verbose "All Parameter Present: False"
-        $response = switch ($null -eq $userid ) {
-          True {
-            Write-Verbose "UserID Present: False"
-            Invoke-WebRequest @webRequest
-          }
-          False {
-            Write-Verbose "UserID Present: True"
-            $body.scope = 'USER'
-            foreach ($id in $userID){
-              $body.ID    = $id
-              $webRequest.Body  = $body
-              Invoke-WebRequest @webRequest
-            }
-          }
-        }
-        (ConvertFrom-Json $response.Content)
-      }
-    }
-    Write-Verbose "PROCESS Block: End"
-  }
-  end {
-    Write-Verbose "END Block: Start"
-    [system.gc]::Collect()
-    Write-Verbose "END Block: End"
+
+  $oktaAPI              = [hashtable]::new()
+  $oktaAPI.All          = $all
+  $oktaAPI.Body         = [hashtable]::new()
+  $oktaAPI.Body.q       = $query
+  $oktaAPI.Body.limit   = $limit
+  $oktaAPI.Body.filter  = $filter
+  $oktaAPI.Endpoint     = 'apps'
+  
+  switch ([wildcardpattern]::ContainsWildcardCharacters($identity)) {
+    $true  {Invoke-OktaAPI @oktaAPI}
+    $false {(Invoke-OktaAPI @oktaAPI).where({$_.Label -eq $Identity -or $_.ID -eq $Identity})}
   }
 }
