7b4607a89a6eb2b38021e1b3ecc71463d2268c13
Jason Robertson <jason@mxcloud.services>
2023-08-08 16:47:16 -0400

    Refactor Add-OktaAppUser To use the helper function Invoke-OktaAPI Enhanced the command to allow for app Id, Name or Label for Identity Renamed the Parameters the follwoing: AppID --> Identity User   --> Member

--- Source/Public/Add-OktaAppUser.ps1
+++ Source/Public/Add-OktaAppUser.ps1
@@ -12,56 +12,29 @@ function Add-OktaAppUser {
     [parameter(Mandatory)]
     [string[]]$Member
   )
-  begin {
-    try {
-      $appID = (Get-OktaApp -Identity $Identity).id
-    }
-    catch {
-      
-    }
-    
-  }
+  begin {}
   process {
-    foreach ($user in $member){
-      $oktaUser = Get-OktaUser -Identity $User
-      Try {
-        #region Add Okta user to Okta App
+    $appID = (Get-OktaApp -Identity $Identity).id
+    if ($appID) {
+      foreach ($user in $member){
+        $oktaUser = Get-OktaUser -Identity $User
         Switch ($oktaUser.status){
-          Default       {
+          DEPROVISIONED {
+            Write-Warning "$ID is deprovisioned, skipping user"
+          }
+          Default { 
             $oktaAPI            = [hashtable]::new()
             $oktaAPI.Body       = [hashtable]::new()
             $oktaAPI.Body.id    = $oktaUser.Id
             $oktaAPI.Body.scope = 'USER'
-            $oktaAPI.Endpoint   = "apps/" 
+            $oktaAPI.Method     = 'POST'
+            $oktaAPI.Endpoint   = "apps/$appid/users" 
 
             Invoke-OktaAPI @oktaAPI
-
-            #region Build the Rest Method
-            Write-Verbose "Build RestMethod Params: Start"
-            $webRequest             = [hashtable]::new()
-            $webRequest.Uri         = "$oktaUrl/apps/$Identity/users"
-            $webRequest.Body        = ConvertTo-JSON $body
-            $webRequest.Method      = 'POST'
-            $webRequest.Headers     = $headers
-            $webRequest.ContentType = 'application/json'
-            Write-Verbose "Build RestMethod Params: Complete"
-            Try{
-              Write-Verbose "Invoke RestMethod"
-              Invoke-WebRequest @webRequest
-            }
-            Catch {
-              Write-Warning $PSItem.Exception.Message
-            }
-            #endregion
-           }
-          Deprovisioned { Write-Warning "$ID is deprovisioned, skipping user" }
+          }
         }
-        #endregion
       }
-      catch{}
     }
   }
-  end {
-    [system.gc]::Collect();
-  }
+  end {}
 }
