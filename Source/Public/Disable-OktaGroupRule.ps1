2ab5c5c8b8bc9a15234b2a25b5e8d268f3bce89f
Jason Robertson <jason@mxcloud.services>
2023-08-15 07:15:29 -0400

    Refactor Disable-OktaGroupRule -- Changed RuleID parameter to Identity -- Removed the begin, process and end blocks -- Simplified code with helper function Invoke-OktaAPI

--- Source/Public/Disable-OktaGroupRule.ps1
+++ Source/Public/Disable-OktaGroupRule.ps1
@@ -1,45 +1,18 @@
 function Disable-OktaGroupRule {
-  [CmdletBinding(DefaultParameterSetName ='RuleID')]
+  [CmdletBinding(DefaultParameterSetName ='Identity')]
   param (
-    [parameter(ParameterSetName='RuleID')]
-    [string]$RuleID
+    [parameter(ParameterSetName='Identity')]
+    [string]$Identity
   )
-  begin {
-    #region Static Variables
-    #oktaPreview switch used to define either okta.com or oktapreview.com
-    $oktaUrl = Test-OktaConnection
-    #endregion
-    #region Build the headers for the web request
-    $headers                  = [hashtable]::new()
-    $headers.Accept           = 'application/json'
-    $headers.Authorization    = Convert-OktaAPIToken
-    #endregion
-    #region Build the body of the web request
-    Write-Verbose 'Build the body of the web request'
-    $body         = [hashtable]::new()
-    #endregion
-    #region Build the Web Request
-    Write-Verbose 'Building the web request.'
-    $webRequest                 = [hashtable]::new()
-    $webRequest.Uri             = "$oktaUrl/api/v1/groups/rules/$RuleID/lifecycle/deactivate"
-    $webRequest.Body            = $body
-    $webRequest.Method          = 'POST'
-    $webRequest.Headers         = $headers
-    $webRequest.UseBasicParsing = $true
-    #endregion
+  $ruleID = (Get-OktaGroupRule -Identity $Identity).where({$_.Name -eq $identity}).id
+
+  $oktaAPI = [hashtable]::new()
+  $oktaAPI.Method = 'POST'
+  $oktaAPI.Endpoint = "groups/rules/$ruleID/lifecycle/deactivate"
+  try {
+    Invoke-OktaAPI @oktaAPI
   }
-  process {
-    Write-Verbose 'Send Web Query: Start'
-    $response = Invoke-WebRequest @webRequest
-    switch ($response.StatusCode) {
-      204 {
-        Write-Verbose "Send Web Query: Success"
-        Write-Output  "Disabled $RuleID successfully"
-        Write-Verbose "Send Web Query: End"
-      }
-    }
-  }
-  end {
-    [system.gc]::Collect();
+  catch {
+    Write-Error $PSItem.Exception.Message
   }
 }
