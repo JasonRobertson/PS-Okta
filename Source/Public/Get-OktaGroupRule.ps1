c51904a58c542fabbbebbca9a743aeb25875a1f9
Jason Robertson <jason@mxcloud.services>
2023-08-15 07:01:41 -0400

    Refactor of Get-OktaGroupRule -- Removed Status as the funcationlity is not supported by the endpoint. To include this functionality would require querying  all of the group rules and then filter. -- Replaced RuleID and RuleName parameters with Identity. -- Updated the Limit maximum from 300 to 200 per Okta documentation. -- Removed the Being, Process and End Block statements. No pipeline acceptance is needed for the command.

--- Source/Public/Get-OktaGroupRule.ps1
+++ Source/Public/Get-OktaGroupRule.ps1
@@ -1,107 +1,44 @@
 function Get-OktaGroupRule {
-  [CmdletBinding(DefaultParameterSetName ='RuleName')]
+  [CmdletBinding(DefaultParameterSetName ='Default')]
   param (
-    [parameter(ParameterSetName='RuleID')]
-    [string]$RuleID,
-    #[switch]$Search,
-    [parameter(ParameterSetName='RuleName')]
-    [string]$RuleName,
-    [ValidateSet('ACTIVE','INACTIVE')]
-    [string]$Status,
-    [ValidateRange(1,300)]
+    [parameter(ParameterSetName='Identity')]
+    [string]$Identity,
+    [ValidateRange(1,200)]
     [int]$Limit = 50,
     [switch]$MapGroupName,
     [switch]$All
   )
-  begin {
-    #region Static Variables
-    #Verify the connection has been established
-    $oktaUrl = Test-OktaConnection
-    #region Build the headers
-    $headers                  = [hashtable]::new()
-    $headers.Accept           = 'application/json'
-    $headers.Authorization    = Convert-OktaAPIToken
-    #endregion
-    #region Build the body of the web request
-    Write-Verbose 'Build the body of the web request'
-    $body         = [hashtable]::new()
-    $body.limit   = switch ($All) {
-                      True  {200}
-                      False {$Limit}
-                    }
-    switch ($MapGroupName){
-      True {$body.expand  = 'groupIdToGroupNameMap'}
-    }
-    #If statements build for the body properties Status and Search for Group name
-    if ($status)
-      { $body.filter = "status=$Status" }
-    if ($ruleName)
-      { $body.search = $ruleName }
+  
+  $oktaApi              = [hashtable]::new()
+  $oktaApi.All          = $all
+  $oktaApi.Body         = [hashtable]::new()
+  $oktaAPI.Body.limit   = $limit
+  $oktaAPI.Body.expand  = if ($mapGroupName) {'groupIdToGroupNameMap'}
+  $oktaAPI.Body.search  = $Identity
+  $oktaApi.Endpoint     = "groups/rules"
+  
+  $response = (Invoke-OktaAPI @oktaApi)
 
-    #endregion
-    #region Build the Web Request
-    $webRequest                 = [hashtable]::new()
-    $webRequest.Uri             = Switch($PSCmdlet.ParameterSetName){
-                                    RuleName   { "$oktaURL/v1/groups/rules"}
-                                    RuleID     { "$oktaURL/v1/groups/rules/$ruleID"}
-                                  }
-    $webRequest.Body            = $body
-    $webRequest.Method          = 'GET'
-    $webRequest.Headers         = $headers
-    $webRequest.UseBasicParsing = $true
-    #endregion
-  }
-  process {
-    Try {
-      $output = switch ($All) {
-        false {
-          Try {
-            $response = Invoke-WebRequest @webRequest
-            ConvertFrom-Json $response.Content
-          }
-          Catch [System.Net.WebResponse] {
-            Write-Warning $PSItem.Exception.Message
-          }
-        }
-        true {
-          do{
-            $response = Invoke-WebRequest @webRequest
-            $webRequest.Uri = $response.RelationLink.next #RelationLink is the recommended approach for pagination from Okta.
-            $webRequest.Remove('Body')
-            ConvertFrom-Json $response.Content
-          } until (-not $response.RelationLink.next)
+  foreach ($groupRule in $response) {
+    $assingedToGroups = switch ($MapGroupName) {
+      False { $groupRule.actions.assignUserToGroups.groupIds }
+      True  { 
+        foreach ($groupID in $groupRule.actions.assignUserToGroups.groupIds){
+          -join ($groupRule._embedded.groupIdToGroupNameMap.$($groupID), ' [', $groupID,']')
         }
       }
     }
-    Catch {
-      Write-Error $PSItem.Exception.Message
-      Write-Output $PSItem.Exception.ItemName
+
+    [PSCustomObject][Ordered]@{
+      Name              = $groupRule.Name
+      ID                = $groupRule.ID
+      Status            = $groupRule.Status
+      Conditions        = $groupRule.conditions.expression.value
+      AssignedToGroups  = $assingedToGroups
+      ExcludeUsers      = $groupRule.conditions.people.users.exclude
+      ExcludeGroups     = $groupRule.conditions.people.groups.exclude
+      Created           = $groupRule.Created
+      LastUpdated       = $groupRule.LastUpdated
     }
-    if ($output){
-      foreach ($groupRule in $output){
-        $assingedToGroups = switch ($MapGroupName) {
-          True  {
-            foreach ($groupID in $groupRule.actions.assignUserToGroups.groupIds){
-              -join ($groupRule._embedded.groupIdToGroupNameMap.$($groupID), ' (', $groupID,')')
-            }
-          }
-          False { $groupRule.actions.assignUserToGroups.groupIds }
-        }
-        [PSCustomObject][Ordered]@{
-          Name              = $groupRule.Name
-          ID                = $groupRule.ID
-          Status            = $groupRule.Status
-          Conditions        = $groupRule.conditions.expression.value
-          AssignedToGroups  = $assingedToGroups
-          ExcludeUsers      = $groupRule.conditions.people.users.exclude
-          ExcludeGroups     = $groupRule.conditions.people.groups.exclude
-          Created           = $groupRule.Created
-          LastUpdated       = $groupRule.LastUpdated
-        }
-      }
-    }
-  }
-  end {
-    [system.gc]::Collect();
   }
 }
