93bdb11bc5bac52e08dd177dc1650f9b8dccbc2a
Jason Robertson <jason@mxcloud.services>
2023-08-25 17:51:55 -0400

    Refactor Get-OktaAppGroup Simplified with helper function Invoke-OktaAPI

--- Source/Public/Get-OktaAppGroup.ps1
+++ Source/Public/Get-OktaAppGroup.ps1
@@ -2,95 +2,20 @@ function Get-OktaAppGroup {
   [CmdletBinding(DefaultParameterSetName='Limit')]
   param (
     [parameter(Mandatory)]
-    [alias('AppID')]
-    [string]$ID,
-    [parameter(ParameterSetName='GroupID')]
-    [string]$GroupID,
-    [parameter(ParameterSetName='Limit')]
+    [string]$Identity,
     [ValidateRange(1,500)]
-    [int]$Limit,
-    [switch]$Profile,
-    [parameter(ParameterSetName='Limit')]
+    [int]$Limit=500,
     [switch]$all
   )
-  begin {
-    Write-Verbose "BEGIN Block: Start"
-    #Verify the connection has been established
-    $oktaUrl = Test-OktaConnection
-    #region Build the headers
-    $headers                  = [hashtable]::new()
-    $headers.Accept           = 'application/json'
-    $headers.Authorization    = Convert-OktaAPIToken
-    #endregion
-    Write-Verbose "Build Headers: Complete"
-    #endregion
-    #region Build Body
-    $body       = [hashtable]::new()
-    $body.limit = switch ($Limit -ge 1 -or $limit -gt 200){
-                    True  {$Limit}
-                    False {200}
-                  }
-    Write-Verbose $($body.limit)
-    #endregion
-    #region Build Web Request
-    $webRequest                 = [hashtable]::new()
-    $webRequest.Uri             = "$oktaURL/apps/$Id/groups"
-    $webRequest.Method          = 'GET'
-    $webRequest.Headers         = $headers
-    $webRequest.UseBasicParsing = $true
-    $webRequest.Body            = $body
-    #endregion
-  Write-Verbose "BEGIN Block: End"
-  }
-  process {
-    Write-Verbose "PROCESS Block: Start"
-    switch ($all) {
-      True {
-        Write-Verbose "All Parameter Present: True"
-        do{
-          $response = Invoke-WebRequest @webRequest
-          $webRequest.Uri = $response.RelationLink.next #RelationLink is the recommended approach for pagination from Okta.
-          $webRequest.Remove('Body')
-          foreach ($entry in (ConvertFrom-Json $response.Content)) {
-            switch ($Profile) {
-              true  {(Get-OktaGroup -GroupID $entry.ID).Profile}
-              false {Get-OktaGroup -GroupID $entry.ID}
-            }
-          }
-          Write-Verbose "NextLink present: True"
-        } until (-not $response.RelationLink.next)
-        Write-Verbose "NextLink present: False"
-      }
-      False {
-        Write-Verbose "All Parameter Present: False"
-        $response = switch ($null -eq $groupID ) {
-          True {
-            Write-Verbose "GroupID Present: False"
-            Invoke-WebRequest @webRequest
-          }
-          False {
-            Write-Verbose "GroupID Present: True"
-            #$body.scope = 'Group'
-            foreach ($id in $groupID){
-              $body.ID    = $id
-              $webRequest.Body  = $body
-              Invoke-WebRequest @webRequest
-            }
-          }
-        }
-        foreach ($entry in (ConvertFrom-Json $response.Content)) {
-          switch ($Profile) {
-            true  {(Get-OktaGroup -GroupID $entry.ID).Profile}
-            false {Get-OktaGroup -GroupID $entry.ID}
-          }
-        }
-      }
-    }
-    Write-Verbose "PROCESS Block: End"
-  }
-  end {
-    Write-Verbose "END Block: Start"
-    [system.gc]::Collect()
-    Write-Verbose "END Block: End"
+  $appID = (Get-OktaApp -Identity $Identity).id
+
+  if ($appID) {
+    $oktaAPI            = [hashtable]::new()
+    $oktaAPI.All        = $all
+    $oktaAPI.Body       = [hashtable]::new()
+    $oktaAPI.Body.limit = $limit
+    $oktaAPI.Endpoint   = "/apps/$appID/groups"
+
+    Invoke-OktaAPI @oktaAPI
   }
 }
