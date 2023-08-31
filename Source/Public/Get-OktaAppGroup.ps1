c5dfd92b254abff0281d1586b3284d4140774fa4
Jason Robertson <jason@mxcloud.services>
2023-08-30 23:30:49 -0400

    Updated the DefaultParameterSetName Added logic for Group

--- Source/Public/Get-OktaAppGroup.ps1
+++ Source/Public/Get-OktaAppGroup.ps1
@@ -1,21 +1,31 @@
 function Get-OktaAppGroup {
-  [CmdletBinding(DefaultParameterSetName='Limit')]
+  [CmdletBinding(DefaultParameterSetName='Default')]
   param (
     [parameter(Mandatory)]
     [string]$Identity,
+    [string]$Group,
     [ValidateRange(1,500)]
     [int]$Limit=500,
     [switch]$all
   )
   $appID = (Get-OktaApp -Identity $Identity).id
-
   if ($appID) {
+    if ($group) { 
+      $groupID = (Get-OktaGroup -Identity $group).id 
+      if ($groupID -gt 1) {
+        
+      }
+    }
+    $endpoint = switch ($null -eq $GroupID ) {
+      true  {"/apps/$appID/groups"}
+      false {"/apps/$appID/groups/$groupID"}
+    }
     $oktaAPI            = [hashtable]::new()
     $oktaAPI.All        = $all
     $oktaAPI.Body       = [hashtable]::new()
     $oktaAPI.Body.limit = $limit
-    $oktaAPI.Endpoint   = "/apps/$appID/groups"
-
-    Invoke-OktaAPI @oktaAPI
+    $oktaAPI.Endpoint   = $endpoint
+    
+    (Invoke-OktaAPI @oktaAPI) | Select-Object -ExpandProperty profile -Property * -ExcludeProperty Profile,_links
   }
 }
