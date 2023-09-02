2651160902b505f565d8684ac17ea5ef0cdb8c6a
Jason Robertson <jason@mxcloud.services>
2023-09-02 16:36:21 -0400

    Update to Invoke-OktaOAuth

--- Source/Private/Invoke-OktaOAuth.ps1
+++ Source/Private/Invoke-OktaOAuth.ps1
@@ -6,21 +6,25 @@ function Invoke-OktaOAuth {
     [parameter(Mandatory)]
     $ClientID,
     [string]$RedirectUri = 'http://localhost:8080/authorization-code/callback',
-    [string[]]$Scopes
+    [string[]]$Scopes,
+    [switch]$OktaPreview
   )
 
   #Proof Key for Code Exchange (PKCE)
-  $private:PKCE = New-OktaPKCE
+  $script:PKCE = New-OktaPKCE
 
   # Random generation that is URL safe
   $state = -join (((48..57) * 4) + ((65..90) * 4) + ((97..122) * 4) | Get-Random -Count 28 | ForEach-Object { [char]$_ })
 
   $webRequest                             = [hashtable]::new()
-  $webRequest.URI                         = "https://$domain/oauth2/v1/authorize"
+  $webRequest.URI                         = switch ($OktaPreview) {
+                                              true  {"https://$domain.oktapreview.com/oauth2/v1/authorize"}
+                                              false {"https://$domain.okta.com/oauth2/v1/authorize"}
+                                            }
   $webRequest.Method                      = 'GET'
   $webRequest.Body                        = [hashtable]::new()
   $webRequest.Body.client_id              = $clientID
-  $webRequest.Body.code_challenge         = $private:PKCE.CodeChallenge
+  $webRequest.Body.code_challenge         = $script:PKCE.CodeChallenge
   $webRequest.Body.code_challenge_method  = 'S256'
   $webRequest.Body.scope                  = $Scopes
   $webRequest.Body.redirect_uri           = $RedirectUri
