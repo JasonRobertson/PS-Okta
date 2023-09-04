a1240ee554977106d953a2fa7c350bb8e98d855f
Jason Robertson <jason@mxcloud.services>
2023-09-03 22:16:48 -0400

    Create Enable-OktaBehaviorRule

---
+++ Source/Public/Enable-OktaBehaviorRule.ps1
@@ -0,0 +1,29 @@
+function Enable-OktaBehaviorRule {
+  [cmdletbinding()]
+  param(
+    [string]$Identity
+  )
+  if ([wildcardpattern]::ContainsWildcardCharacters($identity)){
+    $message = {}.invoke()
+    $message.Add('Wildcard is not supported; verify the identity matches one of the examples:')
+    $message.Add('ID:   0oa786gznlVSf15sC5d7')
+    $message.Add('Name: Velocity')
+
+    $errorRecord = [System.Management.Automation.ErrorRecord]::new(
+    [Exception]::new(($message | Out-String)),
+    'ErrorID',
+    [System.Management.Automation.ErrorCategory]::ObjectNotFound,
+    'Okta'
+    )
+    $pscmdlet.ThrowTerminatingError($errorRecord)
+  }
+  else {
+    $behaviorID = (Get-OktaBehaviorRule -Identity $Identity).ID
+    if ($behaviorID.count -eq 1) {
+      $oktaAPI          = [hashtable]::new()
+      $oktaAPI.Method   = 'POST'
+      $oktaAPI.Endpoint = "behaviors/$behaviorID/lifecycle/activate"
+      (Invoke-OktaAPI @oktaAPI) | Select-Object -Property * -ExpandProperty Settings -ExcludeProperty Settings, _links
+    }
+  }
+}
