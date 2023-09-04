ccacc6383e5301490a775a1ad5c10b2ae9297423
Jason Robertson <jason@mxcloud.services>
2023-09-03 22:17:29 -0400

    Create Remove-OktaBehaviorRule.ps1

--- Source/Public/Remove-OktaBehaviorRule.ps1
+++ Source/Public/Remove-OktaBehaviorRule.ps1
@@ -3,10 +3,27 @@ function Remove-OktaBehaviorRule {
   param(
     [string]$Identity
   )
-  $behaviorID = (Get-OktaBehaviorRule -Identity $Identity).ID
-  
-  $oktaAPI          = [hashtable]::new()
-  $oktaAPI.Method   = 'DELETE'
-  $oktaAPI.Endpoint = "behaviors/$behaviorID"
-  Invoke-OktaAPI @oktaAPI
+  if ([wildcardpattern]::ContainsWildcardCharacters($identity)){
+    $message = {}.invoke()
+    $message.Add('Wildcard is not supported; verify the identity matches one of the examples:')
+    $message.Add('ID:   0oa786gznlVSf15sC5d7')
+    $message.Add('Name: Velocity')
+
+    $errorRecord = [System.Management.Automation.ErrorRecord]::new(
+    [Exception]::new(($message | Out-String)),
+    'ErrorID',
+    [System.Management.Automation.ErrorCategory]::ObjectNotFound,
+    'Okta'
+    )
+    $pscmdlet.ThrowTerminatingError($errorRecord)
+  }
+  else {
+    $behaviorID = (Get-OktaBehaviorRule -Identity $Identity).ID
+    if ($behaviorID.count -eq 1) {
+      $oktaAPI          = [hashtable]::new()
+      $oktaAPI.Method   = 'DELETE'
+      $oktaAPI.Endpoint = "behaviors/$behaviorID"
+      Invoke-OktaAPI @oktaAPI
+    }
+  }
 }
