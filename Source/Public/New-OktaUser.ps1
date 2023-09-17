2742d035705f28b6f2383ea809b9e789a34aded5
Jason Robertson <jason@mxcloud.services>
2023-09-17 09:07:45 -0400

    Refactor New-OktaUser.ps1 Replaced if statements with $PSbountParameters switch for parameters

--- Source/Public/New-OktaUser.ps1
+++ Source/Public/New-OktaUser.ps1
@@ -1,113 +1,117 @@
 function New-OktaUser {
-    [cmdletbinding()]
-    param(
-        #Unique identifier for the user
-        [parameter(Mandatory)]
-        [ValidateLength(1,100)]
-        [string]$UserName,
-        #Given name of the user
-        [parameter(Mandatory)]
-        [ValidateLength(1,50)]
-        [string]$FirstName,
-        #Family name of the user
-        [parameter(Mandatory)]
-        [ValidateLength(1,50)]
-        [string]$LastName,
-        #Middle name(s) of the user
-        [string]$MiddleName,
-        #Casual way to address the user in real life
-        [string]$NickName,
-        #Name of the user, suitable for display to end users
-        [string]$DisplayName,
-        #Primary email address of the user
-        [ValidateLength(5,100)]
-        [string]$Email,
-        #SecondEmail address of the user typically used for account recovery
-        [string]$SecondEmail,
-        #URL of the user's online profile (for example: a web page)
-        [string]$ProfileUrl,
-        #User's preferred written or spoken languages
-        [string]$PreferredLanguage,
-        #Used to describe the organization to user relationship such as "Employee" or "Contractor"
-        [string]$UserType,
-        #Name of the user's organization
-        [string]$Organization,
-        #User's title, such as "Vice President"
-        [string]$Title,
-        #Name of the user's division
-        [string]$Division,
-        #Name of the user's department
-        [string]$Department,
-        #Name of a cost center assigned to user
-        [string]$CostCenter,
-        #Organization or company assigned unique identifier for the user
-        [string]$EmployeeNumber,
-        #Primary phone number of the user such as home number
-        [ValidateLength(1,100)]
-        [string]$PrimaryPhone,
-        #Mobile phone number of the user
-        [string]$MobilePhone,
-        #Full street address component of the user's address
-        [string]$StreetAddress,
-        #City or locality component of the user's address (locality)
-        [string]$City,
-        #State or region component of the user's address (region)
-        [string]$State,
-        #ZIP code or postal code component of the user's address (postalCode)
-        [string]$ZipCode,
-        #Country name component of the user's address (country)
-        [string]$CountryCode,
-        #Honorific prefix(es) of the user, or title in most Western languages
-        [string]$honorificPrefix,
-        #Honorific suffix(es) of the user
-        [string]$honorificSuffix,
-        #Mailing address component of the user's address
-        [string]$PostAddress,
-        #DisplayName of the user's manager
-        [string]$Manager,
-        #id of a user's manager
-        [string]$ManagerID,
-        #custom attributes created by the organization
-        $CustomAttribute = $null
-    )
-    $body    = [hashtable]::new()
-    $payload = [hashtable]::new()
+  [cmdletbinding()]
+  param(
+    #Unique identifier for the user
+    [parameter(Mandatory)]
+    [ValidateLength(1,100)]
+    [string]$UserName,
+    #Given name of the user
+    [parameter(Mandatory)]
+    [ValidateLength(1,50)]
+    [string]$FirstName,
+    #Family name of the user
+    [parameter(Mandatory)]
+    [ValidateLength(1,50)]
+    [string]$LastName,
+    #Middle name(s) of the user
+    [string]$MiddleName,
+    #Casual way to address the user in real life
+    [string]$NickName,
+    #Name of the user, suitable for display to end users
+    [string]$DisplayName,
+    #Primary email address of the user
+    [ValidateLength(5,100)]
+    [string]$Email,
+    #SecondEmail address of the user typically used for account recovery
+    [string]$SecondEmail,
+    #URL of the user's online profile (for example: a web page)
+    [string]$ProfileUrl,
+    #User's preferred written or spoken languages
+    [string]$PreferredLanguage,
+    #Used to describe the organization to user relationship such as "Employee" or "Contractor"
+    [string]$UserType,
+    #Name of the user's organization
+    [string]$Organization,
+    #User's title, such as "Vice President"
+    [string]$Title,
+    #Name of the user's division
+    [string]$Division,
+    #Name of the user's department
+    [string]$Department,
+    #Name of a cost center assigned to user
+    [string]$CostCenter,
+    #Organization or company assigned unique identifier for the user
+    [string]$EmployeeNumber,
+    #Primary phone number of the user such as home number
+    [ValidateLength(1,100)]
+    [string]$PrimaryPhone,
+    #Mobile phone number of the user
+    [string]$MobilePhone,
+    #Full street address component of the user's address
+    [string]$StreetAddress,
+    #City or locality component of the user's address (locality)
+    [string]$City,
+    #State or region component of the user's address (region)
+    [string]$State,
+    #ZIP code or postal code component of the user's address (postalCode)
+    [string]$ZipCode,
+    #Country name component of the user's address (country)
+    [string]$CountryCode,
+    #Honorific prefix(es) of the user, or title in most Western languages
+    [string]$honorificPrefix,
+    #Honorific suffix(es) of the user
+    [string]$honorificSuffix,
+    #Mailing address component of the user's address
+    [string]$PostAddress,
+    #DisplayName of the user's manager
+    [string]$Manager,
+    #id of a user's manager
+    [string]$ManagerID,
+    #custom attributes created by the organization
+    $CustomAttribute = $null
+  )
+  $body    = [hashtable]::new()
+  $payload = [hashtable]::new()
 
-    if ($UserName)          {$payload.login             = $userName}
-    if ($FirstName)         {$payload.firstName         = $firstName}
-    if ($LastName)          {$payload.lastName          = $lastName}
-    if ($MiddleName)        {$payload.middleName        = $middleName}
-    if ($NickName)          {$payload.nickName          = $nickName}
-    if ($DisplayName)       {$payload.displayName       = $displayName}
-    if ($Email)             {$payload.email             = $email}
-    if ($SecondEmail)       {$payload.secondEmail       = $secondEmail}
-    if ($ProfileUrl)        {$payload.profileUrl        = $profileUrl}
-    if ($PreferredLanguage) {$payload.preferredLanguage = $preferredLanguage}
-    if ($UserType)          {$payload.userType          = $userType}
-    if ($Organization)      {$payload.organization      = $organization}
-    if ($Title)             {$payload.title             = $title}
-    if ($Division)          {$payload.division          = $division}
-    if ($Department)        {$payload.department        = $department}
-    if ($CostCenter)        {$payload.costCenter        = $costCenter}
-    if ($EmployeeNumber)    {$payload.employeeNumber    = $employeeNumber}
-    if ($MobilePhone)       {$payload.mobilePhone       = $mobilePhone}
-    if ($StreetAddress)     {$payload.streetAddress     = $streetAddress}
-    if ($City)              {$payload.city              = $city}
-    if ($State)             {$payload.state             = $state}
-    if ($ZipCode)           {$payload.zipCode           = $ZipCode}
-    if ($CountryCode)       {$payload.countryCode       = $countryCode}
-    if ($honorificPrefix)   {$payload.honorificPrefix   = $honorificPrefix}
-    if ($honorificSuffix)   {$payload.honorificSuffix   = $honorificSuffix}
-    if ($PostAddress)       {$payload.postAddress       = $postAddress}
-    if ($Manager)           {$payload.manager           = $manager}
-    if ($ManagerID)         {$payload.managerId         = $managerID}
+  switch ($PSBoundParameters.Keys) {
+    UserName            {$payload.login             = $userName}
+    FirstName           {$payload.firstName         = $firstName}
+    LastName            {$payload.lastName          = $lastName}
+    MiddleName          {$payload.middleName        = $middleName}
+    NickName            {$payload.nickName          = $nickName}
+    DisplayName         {$payload.displayName       = $displayName}
+    Email               {$payload.email             = $email}
+    SecondEmail         {$payload.secondEmail       = $secondEmail}
+    ProfileUrl          {$payload.profileUrl        = $profileUrl}
+    PreferredLanguage   {$payload.preferredLanguage = $preferredLanguage}
+    UserType            {$payload.userType          = $userType}
+    Organization        {$payload.organization      = $organization}
+    Title               {$payload.title             = $title}
+    Division            {$payload.division          = $division}
+    Department          {$payload.department        = $department}
+    CostCenter          {$payload.costCenter        = $costCenter}
+    EmployeeNumber      {$payload.employeeNumber    = $employeeNumber}
+    MobilePhone         {$payload.mobilePhone       = $mobilePhone}
+    StreetAddress       {$payload.streetAddress     = $streetAddress}
+    City                {$payload.city              = $city}
+    State               {$payload.state             = $state}
+    ZipCode             {$payload.zipCode           = $ZipCode}
+    CountryCode         {$payload.countryCode       = $countryCode}
+    honorificPrefix     {$payload.honorificPrefix   = $honorificPrefix}
+    honorificSuffix     {$payload.honorificSuffix   = $honorificSuffix}
+    PostAddress         {$payload.postAddress       = $postAddress}
+    Manager             {$payload.manager           = $manager}
+    ManagerID           {$payload.managerId         = $managerID}
+  }
+  $body.profile = switch ($null -eq $CustomAttribute) {
+                    True    {$payload}
+                    False   {$payload + $customAttribute}
+                  }
 
-    $body.profile = if ($customattribute) {$payload + $customAttribute} else {$payload}
+  $oktaAPI          = [hashtable]::new()
+  $oktaAPI.Body     = $body
+  $oktaAPI.Method   = 'POST'
+  $oktaAPI.Endpoint = "/users"
 
-    $oktaAPI          = [hashtable]::new()
-    $oktaAPI.Method   = 'POST'
-    $oktaAPI.Body     = $body
-    $oktaAPI.Endpoint = "/users"
-
-    Invoke-OktaAPI @oktaAPI
+  Invoke-OktaAPI @oktaAPI
 }
