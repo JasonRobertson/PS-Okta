8b339d564fc39e3af0489323ef98b9abb705e1e1
Jason Robertson <jason@mxcloud.services>
2023-09-17 22:21:23 -0400

    Create Reovke-OktaSupportAccess.ps1

---
+++ Source/Public/Revoke-OktaSupportAccess.ps1
@@ -0,0 +1,8 @@
+function Revoke-OktaSupportAccess {
+  [cmdletbinding()]
+  param()
+  $oktaAPI          = [hashtable]::new()
+  $oktaAPI.Method   = 'POST'
+  $oktaAPI.EndPoint = 'org/privacy/oktaSupport/revoke'
+  Invoke-OktaAPI @oktaAPI | Select-Object -ExcludeProperty _links
+}
