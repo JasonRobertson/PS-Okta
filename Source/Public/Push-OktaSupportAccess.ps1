948e8e5c2fab4d60e88c6b6f5107348c7c8297e7
Jason Robertson <jason@mxcloud.services>
2023-09-17 22:19:04 -0400

    Rename Disable-OktaSupportAccess to Revoke-OktaSupportAccess

--- Source/Public/Push-OktaSupportAccess.ps1
+++ Source/Public/Push-OktaSupportAccess.ps1
@@ -1,8 +1,8 @@
-function Disable-OktaSupportAccess {
+function Push-OktaSupportAccess {
   [cmdletbinding()]
   param()
   $oktaAPI          = [hashtable]::new()
   $oktaAPI.Method   = 'POST'
-  $oktaAPI.EndPoint = 'org/privacy/oktaSupport/revoke'
+  $oktaAPI.EndPoint = 'org/privacy/oktaSupport/extend'
   Invoke-OktaAPI @oktaAPI | Select-Object -ExcludeProperty _links
 }
