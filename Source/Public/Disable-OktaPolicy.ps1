8289839ce4facc08065177734afb4dd875404c97
Jason Robertson <jason@mxcloud.services>
2023-09-20 21:55:45 -0400

    Create Disable-OktaPolicy.ps1

---
+++ Source/Public/Disable-OktaPolicy.ps1
@@ -0,0 +1,18 @@
+function Disable-OktaPolicy {
+  [CmdletBinding()]
+  param(
+    [parameter(ValueFromPipeLine, position=0)]
+    [string]$Identity
+  )
+  process {
+    $oktaAPI.[hashtable]::new()
+    $oktaAPI.Method   = 'POST'
+    $oktaAPI.Endpoint = "policies/$Identity/lifecycle/deactivate"
+    try {
+      Invoke-OktaAPI @oktaAPI
+    }
+    catch {
+      $PSItem.Exception.Message
+    }
+  }
+}
