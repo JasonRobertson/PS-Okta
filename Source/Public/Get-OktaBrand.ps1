e9374d8cc9507f4b48640f3bca62b2fefda8b908
Jason Robertson <jason@mxcloud.services>
2023-09-26 09:44:46 -0400

    Refactor Get-OktaBrand.ps1

--- Source/Public/Get-OktaBrand.ps1
+++ Source/Public/Get-OktaBrand.ps1
@@ -1,29 +1,33 @@
 function Get-OktaBrand {
   [CmdletBinding()]
   param(
-    [string]$Identity
+    [string]$Identity,
+    [ValidateRange(1,200)]
+    [int32]$Limit
   )
-  $response = Invoke-OktaAPI -Endpoint 'brands'
-  if ($identity) {
-    $response = switch ([wildcardpattern]::ContainsWildcardCharacters($identity)) {
-      True    {$response.where({$_.name -like $Identity -or $_.ID -like $Identity})}
-      False   {$response.where({$_.name -eq   $Identity -or $_.ID -eq   $Identity})}
-    }
-    if (-not $response) {
-      $message = {
-        "Failed to retrieve Okta Brand $identity, verify the ID matches one of the examples:"
-        'ID:   0oa786gznlVSf15sC5d7'
-        'Name: dev-56213942_default'
-      }.invoke() | Out-String
+  $oktaAPI            = [hashtable]::new()
+  $oktaAPI.Body       = [hashtable]::new()
+  $oktaAPI.Body.limit = $Limit
+  $oktaAPI.Body.q     = $Identity
+  $oktaAPI.Endpoint   = 'brands'
+ 
+  $response = Invoke-OktaAPI @oktaAPI
+  if (-not $response) {
+    $message = {
+      "Failed to retrieve Okta Brand $identity, verify the ID matches one of the examples:"
+      'ID:   0oa786gznlVSf15sC5d7'
+      'Name: dev-56213942_default'
+    }.invoke() | Out-String
 
-      $errorRecord = [System.Management.Automation.ErrorRecord]::new(
-      [Exception]::new($message),
-      'ErrorID',
-      [System.Management.Automation.ErrorCategory]::ObjectNotFound,
-      'Okta'
-      )
-      $pscmdlet.ThrowTerminatingError($errorRecord)
-    }
+    $errorRecord = [System.Management.Automation.ErrorRecord]::new(
+    [Exception]::new($message),
+    'ErrorID',
+    [System.Management.Automation.ErrorCategory]::ObjectNotFound,
+    'Okta'
+    )
+    $pscmdlet.ThrowTerminatingError($errorRecord)
   }
-  return $response | Select-Object -ExpandProperty defaultApp -ExcludeProperty _links, defaultApp
+  else {
+    return $response | Select-Object -ExpandProperty defaultApp -ExcludeProperty _links, defaultApp
+  }
 }
