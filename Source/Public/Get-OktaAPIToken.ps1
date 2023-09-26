cde44144b5814bbb9ed09d34410fa682a83ace96
Jason Robertson <jason@mxcloud.services>
2023-09-26 09:45:07 -0400

    Refactor Get-OktaAPIToken

--- Source/Public/Get-OktaAPIToken.ps1
+++ Source/Public/Get-OktaAPIToken.ps1
@@ -1,28 +1,31 @@
 function Get-OktaAPIToken {
   [CmdletBinding()]
   param(
-  [string]$Identity
-  )  
-  $response = Invoke-OktaAPI -Endpoint api-tokens
-  if ($identity) {
-    $response = switch ([wildcardpattern]::ContainsWildcardCharacters($identity)) {
-      True    {$response.where({$_.name -like $Identity -or $_.ID -like $Identity})}
-      False   {$response.where({$_.name -eq $Identity -or $_.ID -eq $Identity})}
-    }
-    if (-not $response) {
-      $message = {
-        "Failed to retrieve Okta API Token $identity, verify the ID matches one of the examples:"
-        'ID:   0oa786gznlVSf15sC5d7'
-        'Name: Local Computer'
-      }.invoke() | Out-String
-      $errorRecord = [System.Management.Automation.ErrorRecord]::new(
-      [Exception]::new($message),
-      'ErrorID',
-      [System.Management.Automation.ErrorCategory]::ObjectNotFound,
-      'Okta'
-      )
-      $pscmdlet.ThrowTerminatingError($errorRecord)
-    }
+  [string]$Identity,
+  [ValidateRange(1,200)]
+  [int32]$Limit = 20
+  )
+  $oktaAPI            = [hashtable]::new()
+  $oktaAPI.Body       = [hashtable]::new()
+  $oktaAPI.Body.limit = $Limit
+  $oktaAPI.Body.q     = $Identity
+  $oktaAPI.Endpoint   = 'api-tokens'
+
+  $response = Invoke-OktaAPI @oktaAPI
+  
+  if (-not $response) {
+    $message = {
+      "Failed to retrieve Okta API Token $identity, verify the ID matches one of the examples:"
+      'ID:   0oa786gznlVSf15sC5d7'
+      'Name: Local Computer'
+    }.invoke() | Out-String
+    $errorRecord = [System.Management.Automation.ErrorRecord]::new(
+    [Exception]::new($message),
+    'ErrorID',
+    [System.Management.Automation.ErrorCategory]::ObjectNotFound,
+    'Okta'
+    )
+    $pscmdlet.ThrowTerminatingError($errorRecord)
   }
   return $response
 }
