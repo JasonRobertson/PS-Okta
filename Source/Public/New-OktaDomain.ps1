1a6d62cc60e9010354533f736ce62749ce9c7be1
Jason Robertson <jason@mxcloud.services>
2023-09-28 09:39:38 -0400

    Create New-OktaDomain.ps1

---
+++ Source/Public/New-OktaDomain.ps1
@@ -0,0 +1,24 @@
+function New-OktaDomain {
+  [cmdletbinding()]
+  param(
+    [parameter(Mandatory,Position=0)]
+    [string]$Name,
+    [ValidateSet('Manual', 'Okta Managed')]
+    $CertificateSourceType = 'Okta Managed'
+  )
+  try {
+    $certificateSourceType = $certificateSourceType.Replace(' ','_').ToUpper()
+
+    $oktaAPI                            = [hashtable]::new()
+    $oktaAPI.Body                       = [hashtable]::new()
+    $oktaAPI.Body.domain                = $name
+    $oktaAPI.Body.certificateSourceType = $certificateSourceType
+    $oktaAPI.Method                     = 'POST'
+    $oktaAPI.Endpoint                   = 'domains'
+    
+    Invoke-OktaAPI @oktaAPI | Select-Object -ExcludeProperty _links
+  }
+  catch {
+    Write-Error $PSItem.Exception.Message
+  }
+}
