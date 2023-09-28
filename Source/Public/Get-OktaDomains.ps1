ce628183b203a8440022adced0163a44b13ee781
Jason Robertson <jason@mxcloud.services>
2023-09-28 08:39:20 -0400

    Create Get-OktaDomains.ps1

---
+++ Source/Public/Get-OktaDomains.ps1
@@ -0,0 +1,26 @@
+function Get-OktaDomains {
+  [cmdletbinding()]
+  param(
+    $Identity
+  )
+  try {
+    $oktaAPI          = [hashtable]::new()
+    $oktaAPI.Endpoint = 'domains'
+
+    $response = (Invoke-OktaAPI @oktaAPI).domains
+
+    $output = if ($identity) {
+      switch ([wildcardpattern]::ContainsWildcardCharacters($identity)) {
+        True {$response.Where({$_.Id -like $identity -or $_.Domain -like $identity})}
+        False {$response.Where({$_.Id -eq $identity -or $_.Domain -eq $identity})}
+      }
+    }
+    else {
+      $response
+    }
+    $output | Select-Object -ExcludeProperty _links
+  }
+  catch {
+    Write-Error $PSItem.Exception.Message
+  }
+}
