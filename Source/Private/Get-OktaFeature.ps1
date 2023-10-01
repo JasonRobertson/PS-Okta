9ae1355ae42ee575be3f5c7cb05e39557933bf00
Jason Robertson <jason@mxcloud.services>
2023-10-01 01:32:37 -0400

    Refactor Get-OktaFeature.ps1

--- Source/Private/Get-OktaFeature.ps1
+++ Source/Private/Get-OktaFeature.ps1
@@ -1,31 +1,31 @@
 function Get-OktaFeature {
-  [cmdletbinding(DefaultParameterSetName='Default')]
+  [cmdletbinding(DefaultParameterSetName='List')]
   param(
-    [parameter(ParameterSetName='Identity')]
+    [parameter(Mandatory,ParameterSetName='Identity', Position='0')]
     [string]$Identity,
     [ValidateSet('Enabled','Disabled')]
-    [parameter(ParameterSetName='Default')]
+    [parameter(ParameterSetName='List',Position='0')]
     [string]$Status,
     [ValidateSet('EA','Beta')]
-    [parameter(ParameterSetName='Default')]
+    [parameter(ParameterSetName='List',Position='1')]
     [string]$Stage
   )
   process {
     try {
       $oktaAPI          = [hashtable]::new()
       $oktaAPI.Endpoint = "features/$identity"
-
-      $response = Invoke-OktaAPI @oktaAPI
-      $response = if ($status){
-        $response.where({$_.status -eq $status})
-      }
-      $response = if ($stage) {
-         $response.where({$_.stage.value -eq $Stage}) 
-      }
-      foreach ($entry in $response) {
+      Write-Debug $oktaAPI.Endpoint
+      $response = foreach ($entry in (Invoke-OktaAPI @oktaAPI) ) {
         $entry.stage = $entry.stage.value
         $entry 
       }
+      if ($status){
+        $response = $response.Where({$_.Status -eq $status})
+      }
+      if ($Stage) {
+        $response = $response.Where({$_.Stage -eq $stage})
+      }
+      return $response
     }
     catch {
       Write-Error $PSItem.Exception.Message
