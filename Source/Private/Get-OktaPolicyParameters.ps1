49dd30bd6cc71766f301c9fce3bde049ecbbbbd3
Jason Robertson <jason@mxcloud.services>
2023-11-17 09:46:09 -0500

    Draft for Get-OktaPolicyParameters.ps1

---
+++ Source/Private/Get-OktaPolicyParameters.ps1
@@ -0,0 +1,27 @@
+function Get-OktaPolicyParameters {
+  $dynamicParameters = {}.invoke()
+
+  $includeApp               = [hashtable]::new()
+  $includeApp.Name          = 'IncludeApp'
+  $includeApp.Type          = 'Int32'
+  $includeApp.ValidateRange = 1, 40075
+  $includeApp.Position      = 3
+  $includeApp.DefaultValue  = 805
+
+  $dynamicParameters.Add([PSCustomObject]$includeApp)
+
+  switch -wildcard ($type) {
+    'Authorization Server Policy' {
+
+    }
+    'Access Policy' {
+      
+    }
+    'IDP Discovery' {}
+    'MFA Enroll' {}
+    'Okta Sign On' {}
+    'Password' {}
+    'Profile Enrollment' {}
+  }
+  $dynamicParameters | New-DynamicParameter
+}
