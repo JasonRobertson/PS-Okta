5f376c3124229d325d6d17ca16e3081259ce88d7
jason@mxcloud.services <Jason@mxcloud.services>
2024-01-17 10:30:37 -0500

    Create Remove-OktaGroupMember.ps1

---
+++ Source/Public/Remove-OktaGroupMember.ps1
@@ -0,0 +1,46 @@
+function Remove-OktaGroupMember {
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory)]
+    #Identity parameter accepts ID or Name
+    # ID:   00g786gzjzv02mK3Z5d7
+    # Name: Everyone
+    [string]$Identity,
+    # Member parameter accepts ID, Login or Login Shortname
+    # ID:               00ub0oNGTSWTBKOLGLNR
+    # Login:            isaac.brock@example.com
+    # Login Shortname:  isaac.brock
+    [Parameter(Mandatory)]
+    [string[]]$Member
+  )
+  begin{}
+  process{
+    try {
+      $groupID = (Get-OktaGroup -Identity $Identity).id
+    }
+    catch {
+      Write-Error $PSItem.Exception.Message -ErrorAction Stop
+    }
+      foreach ($user in $member) {
+        try {
+          $oktaUser = Get-OktaUser -Identity $user
+          switch ($oktaUser.status){
+            DEPROVISIONED {
+              Write-Warning "User $user is deprovisioned, skipping user"
+            }
+            Default {
+              $userID           = $oktaUser.Id
+              $oktaAPI          = [hashtable]::New()
+              $oktaAPI.Method   = "DELETE"
+              $oktaAPI.Endpoint = "groups/$groupID/users/$userID"
+              Invoke-OktaAPI @oktaAPI
+            }
+          }
+        }
+        catch {
+          Write-Error $PSItem.Exception.Message
+        }
+      }
+  }
+  end{}
+}
