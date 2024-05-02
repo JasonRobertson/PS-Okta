c44bc460d6f270954ae7cb34bc76e7bee62e3bb6
Jason Robertson <jason.robertson@wework.com>
2024-05-02 10:13:07 -0400

    Create Remove-OktaAppUser.ps1

---
+++ Source/Public/Remove-OktaAppUser.ps1
@@ -0,0 +1,31 @@
+function Remove-OktaAppUser {
+  [cmdletbinding()]
+  param(
+    [parameter(Mandatory,postion=0)]
+    [string]$Identity,
+    [parameter(Mandatory,postion=1)]
+    [string[]]$User,
+    [switch]$SendEmail
+  )
+  begin {
+    $appId = (Get-OktaApp -Identity $Identity).id
+  }
+  process {
+    if ($appID) {
+      try {
+        foreach ($entry in $user) {
+          $userID = (Get-OktaUser -Identity $entry).id
+          if ($userID) {
+            $oktaAPI          = [hashtable]::new()
+            $oktaAPI.endpoint = "apps/$appID/users/$userID"
+            $oktaAPI.Method   = 'DELETE'
+            Invoke-OktaAPI @oktaAPI
+          }
+        }
+      }
+      catch {
+        Write-Error $PSItem.Exception.Message
+      }
+    }
+  }
+}
