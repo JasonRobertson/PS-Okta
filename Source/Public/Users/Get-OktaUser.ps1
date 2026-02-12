db833f6d52ae8bad39e6093b1b32434b46916376
Jason <jason@mxcloud.services>
2026-02-12 11:46:26 -0500

    Add Manager, Country/CountryCode, City, WorkerType, and Domain filters to Get-OktaUser, along with additional profile-based filters (Department, Division, Organization, CostCenter, EmployeeNumber, UserType) and refactor filter construction to support combined queries

--- Source/Public/Users/Get-OktaUser.ps1
+++ Source/Public/Users/Get-OktaUser.ps1
@@ -9,6 +9,43 @@ function Get-OktaUser {
     [parameter(ParameterSetName='Default')]
     [ValidateSet('Active','Provisioned','Deprovisioned','Staged','Recovery','Locked','PasswordExpired')]
     [string]$Status,
+    # Filter by manager (profile.manager)
+    [parameter(ParameterSetName='Default')]
+    [string]$Manager,
+    # Filter by country name (profile.country)
+    [parameter(ParameterSetName='Default')]
+    [string]$Country,
+    # Filter by country code, e.g. US, GB (profile.countryCode)
+    [parameter(ParameterSetName='Default')]
+    [string]$CountryCode,
+    # Filter by city (profile.city)
+    [parameter(ParameterSetName='Default')]
+    [string]$City,
+    # Filter by worker type (profile.workerType)
+    [parameter(ParameterSetName='Default')]
+    [string]$WorkerType,
+    # Filter by email/login domain contained in profile.login
+    [parameter(ParameterSetName='Default')]
+    [string]$Domain,
+    # Recommended additional filters
+    # Department (profile.department)
+    [parameter(ParameterSetName='Default')]
+    [string]$Department,
+    # Division (profile.division)
+    [parameter(ParameterSetName='Default')]
+    [string]$Division,
+    # Organization (profile.organization)
+    [parameter(ParameterSetName='Default')]
+    [string]$Organization,
+    # Cost center (profile.costCenter)
+    [parameter(ParameterSetName='Default')]
+    [string]$CostCenter,
+    # Employee number / ID (profile.employeeNumber)
+    [parameter(ParameterSetName='Default')]
+    [string]$EmployeeNumber,
+    # User type (profile.userType)
+    [parameter(ParameterSetName='Default')]
+    [string]$UserType,
     [parameter(ParameterSetName='Default')]
     [datetime]$LastUpdated,
     [parameter(ParameterSetName='Default')]
@@ -21,6 +58,9 @@ function Get-OktaUser {
     [switch]$All
   )
   begin {
+    # Build Okta filter expression from the various filter parameters
+    $filters = @()
+
     $filterStatus = switch ($status) {
       Active          {'status eq "ACTIVE"'}
       Staged          {'status eq "STAGED"'}
@@ -30,14 +70,31 @@ function Get-OktaUser {
       Deprovisioned   {'status eq "DEPROVISIONED"'}
       PasswordExpired {'status eq "PASSWORD_EXPIRED"'}
     }
-    If ($lastUpdated) {
+    if ($filterStatus)  { $filters += $filterStatus}
+
+    if ($LastUpdated) {
       $filterLastUpdated = "lastUpdated gt ""$(Get-Date $lastUpdated -Format yyyy-MM-ddThh:mm:ss.fffZ)"""
+      $filters += $filterLastUpdated
     }
+
+    if ($Manager)       { $filters += "profile.manager eq ""$Manager""" }
+    if ($Country)       { $filters += "profile.country eq ""$Country""" }
+    if ($CountryCode)   { $filters += "profile.countryCode eq ""$CountryCode""" }
+    if ($City)          { $filters += "profile.city eq ""$City""" }
+    if ($WorkerType)    { $filters += "profile.workerType eq ""$WorkerType""" }
+    if ($Domain)        { $filters += "profile.login co ""$Domain""" }
+    if ($Department)    { $filters += "profile.department eq ""$Department""" }
+    if ($Division)      { $filters += "profile.division eq ""$Division""" }
+    if ($Organization)  { $filters += "profile.organization eq ""$Organization""" }
+    if ($CostCenter)    { $filters += "profile.costCenter eq ""$CostCenter""" }
+    if ($EmployeeNumber){ $filters += "profile.employeeNumber eq ""$EmployeeNumber""" }
+    if ($UserType)      { $filters += "profile.userType eq ""$UserType""" }
+
     $body         = [hashtable]::new()
     $body.limit   = $limit
-    $body.filter  = if ($filterStatus -and $LastUpdated){"$filterStatus and $filterLastUpdated" }
-                    elseif ($filterStatus) {$filterStatus}
-                    elseif ($filterLastUpdated) {$filterLastUpdated}
+    if ($filters.Count -gt 0) {
+      $body.filter = $filters -join ' and '
+    }
   }
   process {
     $oktaAPI          = [hashtable]::new()
